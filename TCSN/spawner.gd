72c96099dde3d04239026c5db33e063338927e51
hasenbraten <richterloris4@outlook.de>
2026-04-17 23:12:46 +0200

    ..

--- TCSN/spawner.gd
+++ TCSN/spawner.gd
@@ -4,17 +4,17 @@ extends Node2D
 @export var spawn_count: int = 4
 @export var spawn_radius: float = 100
 
+
 func _ready():
 	spawn_enemies()
 
 
 func spawn_enemies():
 	for i in range(spawn_count):
-		var enemy = enemy_scene.instantiate()
+		var ai = enemy_scene.instantiate()
 
 		# zufällige Position um den Spawner herum
 		var angle = randf() * TAU
 		var offset = Vector2(cos(angle), sin(angle)) * spawn_radius
-		enemy.global_position = abs(global_position + offset)
-		print((enemy.global_position))
-		#get_parent().add_child(enemy)
+		ai.global_position = abs(global_position + offset)
+		get_parent().add_child(ai)
