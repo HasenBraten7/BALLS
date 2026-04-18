99284f1d434fc9cf18e2f050a4944fb6710bcc16
hasenbraten <richterloris4@outlook.de>
2026-04-18 11:05:42 +0200

    repaired Spawner

---
+++ TCSN/map.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+@export var mob_scene: PackedScene
+var score
--- TCSN/spawner.gd
+++ TCSN/spawner.gd
@@ -2,19 +2,21 @@ extends Node2D
 
 @export var enemy_scene: PackedScene
 @export var spawn_count: int = 4
-@export var spawn_radius: float = 100
+@export var spawn_radius: float = 500
 
 
 func _ready():
-	spawn_enemies()
+	var enemies = [enemy_scene.instantiate(),enemy_scene.instantiate(),enemy_scene.instantiate(),enemy_scene.instantiate()]
+	spawn_enemies(enemies)
 
 
-func spawn_enemies():
-	for i in range(spawn_count):
-		var ai = enemy_scene.instantiate()
-
-		# zufällige Position um den Spawner herum
-		var angle = randf() * TAU
-		var offset = Vector2(cos(angle), sin(angle)) * spawn_radius
-		ai.global_position = abs(global_position + offset)
-		get_parent().add_child(ai)
+func spawn_enemies(enemies):
+	for i in enemies:
+		var ai = i
+		var spawn_location
+		var offset
+		
+		spawn_location = abs(Vector2(randf() * spawn_radius,randf()*spawn_radius))
+		ai.global_position = spawn_location
+		print(ai.global_position)
+		add_child(ai)
